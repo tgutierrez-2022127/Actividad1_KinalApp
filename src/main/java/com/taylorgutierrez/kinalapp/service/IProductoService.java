01ce4307cfcf9f03148d33127bb126173b4bad2f
tgutierrez-2022127 <tgutierrez-2022127@kinal.edu.gt>
2026-03-26 20:11:14 -0600

    feat: definición de interfaz de servicio Producto

---
+++ src/main/java/com/taylorgutierrez/kinalapp/service/IProductoService.java
@@ -0,0 +1,30 @@
+package com.taylorgutierrez.kinalapp.service;
+
+import com.taylorgutierrez.kinalapp.entity.Producto;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IProductoService {
+
+    @Transactional(readOnly = true)
+    List<Producto> listarProductos();
+
+    List<Producto> listaStock(int stock);
+
+    @Transactional(readOnly = true)
+    List<Producto> listarPorStock(int stock);
+
+    Producto guardar(Producto producto);
+
+    Optional<Producto> buscarPorId(Long id);
+
+    Producto actualizar(Long id, Producto producto);
+
+    void eliminar(Long id);
+
+    boolean existePorId(Long id);
+
+    List<Producto> buscarPorStock(int stock);
+}
