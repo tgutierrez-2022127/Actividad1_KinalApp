7b2df4083227314c60be57eaba50f3d04b0cb05f
TuNombre <tuemail@gmail.com>
2026-05-01 16:41:52 -0600

    feat: actualizar repositorios

--- src/main/java/com/taylorgutierrez/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/taylorgutierrez/kinalapp/repository/UsuarioRepository.java
@@ -4,15 +4,9 @@ import com.taylorgutierrez.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
     Optional<Usuario> findByCorreo(String correo);
-
-    boolean existsByCorreo(String correo);
-
-    List<Usuario> findByEstado(Integer estado);
 }
